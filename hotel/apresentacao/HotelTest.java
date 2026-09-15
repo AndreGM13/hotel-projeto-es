58ceea7cfe73bd383d820a4d4e490820b8417ac7
André Garrido <andregarridomacedo@gmail.com>
2026-09-15 17:17:49 -0300

    Initial commit

---
+++ hotel/apresentacao/HotelTest.java
@@ -0,0 +1,18 @@
+package hotel.apresentacao;
+
+import hotel.modelo.*;
+import hotel.negocio.Hotel;
+public class HotelTest {
+    private static int passou = 0;
+    private static int total = 0;
+
+    public static void main(String[] args) {
+        // Exemplo:
+        // testarReservarAptoLivre();
+        // testarReservarAptoOcupadoFalha();
+        // ...
+
+        System.out.println(passou + "/" + total + " testes passaram");
+    }
+    
+}
